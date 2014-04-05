a4dd32037a0895247e51a3652512e00f4bcef01b
spiralilas <spiralilas@yahoo.ca>
2014-04-05 11:19:14 -0400

    initial version

---
+++ README.md
@@ -0,0 +1,5 @@
+##project
+
+**Categories**: education, Dart
+
+**Description**: just another school project
---
+++ build.dart
@@ -0,0 +1,6 @@
+import 'package:polymer/builder.dart';
+        
+main(args) {
+  build(entryPoints: ['web/project.html'],
+        options: parseOptions(args));
+}
